14f24257278444eb320835130b9182d9388a7ec7
Tu FirOcT <hongtupharma@gmail.com>
2023-09-09 16:43:58 +0700

    change UX/UI and switch to typescript, vite

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main class="bg-dark text-light">
+    <nav class="navbar navbar-expand-lg">
+      <div class="container-fluid">
+        <a href="#" class="navbar-brand d-flex align-items-end">
+          <img src="/logo-white.png" alt="logo" class="img-fluid" width="50">
+          <span class="ms-2 h1 text-light fw-medium mb-0">firocther</span>
+        </a>
+        <div class="input-group">
+          <label>
+            <i class="bi bi-search"></i>
+            <input type="text" class="form-control" placeholder="Search city...">
+          </label>
+        </div>
+        <div>
+          <button class="btn btn-primary rounded-pill">
+            <i class="bi bi-geo-fill"></i>
+            Current Location
+          </button>
+        </div>
+      </div>
+    </nav>
+  </main>
+</template>
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import './assets/main.scss'
+import 'bootstrap'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+
+const app = createApp(App)
+
+app.use(createPinia())
+
+app.mount('#app')
